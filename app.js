eee39c2632fd69f7f489c5f8c71b876058fbde11
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-03-13 10:43:28 -0300

    mercadoliebre

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3030;
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "views", "home.html")));
+
+app.get("/ofertas", (req,res) => res.sendFile(path.resolve(__dirname, "views", "ofertas.html")));
+
+app.get("/tienda", (req,res) => res.sendFile(path.resolve(__dirname, "views", "tienda.html")));
+
+app.get("/vender", (req,res) => res.sendFile(path.resolve(__dirname, "views", "vender.html")));
+
+app.get("/ayuda", (req,res) => res.sendFile(path.resolve(__dirname, "views", "ayuda.html")));
+
+app.get("/crear", (req,res) => res.sendFile(path.resolve(__dirname, "views", "crear.html")));
+
+app.get("/ingresar", (req,res) => res.sendFile(path.resolve(__dirname, "views", "ingresar.html")));
+
+app.get("/compras", (req,res) => res.sendFile(path.resolve(__dirname, "views", "compras.html")));
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto 3030 http://localhost:${port}`))
